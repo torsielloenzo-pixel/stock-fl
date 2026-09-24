a617f993d39f1adb9365449f1128a9df02dd57f7
torsielloenzo-pixel <torsielloenzo@gmail.com>
2026-09-24 15:25:47 +0200

    Actualise le cache PWA pour le nouveau design

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='netto-tools-v1';
-const CORE=['./','./index.html','./home.html','./articles.html','./planning.html','./chat.html','./profile.html','./settings.html','./stock-test.html','./manifest.webmanifest','./assets/app-icon.svg','./assets/logo-stock.svg','./assets/logo-planning.svg','./assets/logo-equipe.svg'];
+const CACHE='netto-tools-v2';
+const CORE=['./','./index.html','./home.html','./articles.html','./planning.html','./chat.html','./profile.html','./settings.html','./stock-test.html','./manifest.webmanifest','./design-v2.css','./assets/app-icon.svg','./assets/logo-stock.svg','./assets/logo-planning.svg','./assets/logo-equipe.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
